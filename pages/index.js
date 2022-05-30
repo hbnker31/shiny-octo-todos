329e765912b298ff6423fc282feb3b6959d08dca
Hemant <hemant.bhankhar@okaygo.in>
2022-05-30 19:52:11 +0530

    fixed bugs

--- pages/index.js
+++ pages/index.js
@@ -1,156 +1,158 @@
-import React from 'react';
-import moment from 'moment';
-import _ from 'lodash';
-export default function Home({data}) {
-  const [newtitle, setNewtitle] = React.useState(''); 
-  const [newBody, setNewBody] = React.useState('');
+import React from "react";
+import moment from "moment";
+import Styles from '../styles/Home.module.css'
+import _ from "lodash";
+export default function Home({ data }) {
+  const [newtitle, setNewtitle] = React.useState("");
+  const [newBody, setNewBody] = React.useState("");
   const [initialData, setinitialData] = React.useState(data);
-  const deleteTodo = (id=NULL) => {
+  const deleteTodo = (id = NULL) => {
     document.getElementById(id).remove();
-  }
-  const addTodo = (id=NULL) => {
-    var div = document.createElement('div');
+  };
+  const addTodo = (id = NULL) => {
+    var div = document.createElement("div");
     var getLastId = initialData.reverse()[0].id + 1;
-    div.innerHTML = `<div className="card-body">
-    <h1 className="card-title">${newtitle.charAt(0).toUpperCase() + newtitle.slice(1)}</h1>
-    <p className="card-text">">${newBody.charAt(0).toUpperCase() + newBody.slice(1)}</p>
-    <span>
-      <i className='fa fa-plus' onClick={addTodo(${getLastId+1})}></i>
-    </span>
-    <span>
-      <i className='fa fa-times' onClick={deleteTodo(${getLastId+1})}></i>
-    </span>
-  </div>`;
-  if(id !== NULL){
-    document.getElementById(id).appendChild(div);
-  }else{
-    var newTodo = {
-      id: getLastId,
-      title: newtitle,
-      body: newBody
+    // added key to the div
+    div.innerHTML = `<div className="card-body" key={getLastId} id={getLastId.toString()}> 
+      <h1 className="card-title">${
+        newtitle.charAt(0).toUpperCase() + newtitle.slice(1)
+      }</h1>
+      <p className="card-text">">${
+        newBody.charAt(0).toUpperCase() + newBody.slice(1)
+      }</p>
+      <span>
+        <i className='fa fa-plus' onClick={addTodo(${getLastId + 1})}></i>
+      </span>
+      <span>
+        <i className='fa fa-times' onClick={deleteTodo(${getLastId + 1})}></i>
+      </span>
+    </div>`;
+    if (id !== NULL) {
+      document.getElementById(id).appendChild(div);
+    } else {
+      var newTodo = {
+        id: getLastId,
+        title: newtitle,
+        body: newBody,
+      };
+      // setting newBody and new Title as blank
+      setNewBody("")
+      setNewtitle("")
+      setinitialData((previous) => {
+        previous.push(newTodo);
+      });
     }
-    setinitialData((previous)=>{
-      previous.push(newTodo)
-    })
-  }
-}
-  const findStatus = (id='')=>{
-    switch(id){
+  };
+  const findStatus = (id = "") => {
+    console.log("findStatus",{id})
+    switch (id) {
       case id % 3 === 0:
-        return 'success';
+        return "success";
       case id % 2 == 0:
-        return 'warning';
+        return "warning";
       default:
-        return 'danger';
+        return "danger";
     }
-  }
-  React.useEffect(()=>{
-    var checkTitle =  initialData.filters(data => data.title.contains(newtitle));
-    var checkBody  =  initialData.filters(data => data.body.contains(newBody));
+  };
+  React.useEffect(() => {
+    // fixed check on title and body
+    var checkTitle = initialData.filters((data) =>
+      data.title === newtitle
+    );
+    var checkBody = initialData.filters((data) => data.body === newBody);
 
-    setTimeout(()=>{
-      if(newtitle.length <= 0){
-        return
+    setTimeout(() => {
+      if (newtitle.length <= 0) {
+        return;
       }
-      if(newBody.length <= 0){
-        return
+      if (newBody.length <= 0) {
+        return;
       }
-      if(checkTitle.length > 0){
-        alert('Title is already exist');
+      // checkTitle fixed
+      if (checkTitle.title.length > 0) {
+        alert("Title is already exist");
       }
-      if(checkBody.length > 0){
-        alert('Body is already exist');
+      // checkBody fixed
+      if (checkBody.body.length > 0) {
+        alert("Body is already exist");
       }
-      if(checkTitle.length > 0 && checkBody.length > 0){
-        alert('Title and Body is already exist');
+      // checkTitle & checkBody fixed
+      if (checkTitle.title.length > 0 && checkBody.body.length > 0) {
+        alert("Title and Body is already exist");
       }
-    },400)
-  },[newtitle,newBody,initialData])
-  React.useLayoutEffect(()=>{
-    setinitialData(previous=>_.orderBy(previous, 'title', 'asc'))
-  },[initialData])
+    }, 400);
+    //no need for adding initial data as Dependency
+  }, [newtitle, newBody]);
+  React.useLayoutEffect(() => {
+    setinitialData((previous) => _.orderBy(previous, "title", "asc"));
+  }, [initialData]);
   return (
     <React.Fragment>
       <div className="container">
         <form onSubmit={addTodo()}>
           <div className="form-group">
             <label htmlFor="exampleInputEmail1">Title</label>
-            <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter title" onChange={(e) => setNewtitle(e.target.value)} />
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter title"
+              onChange={(e) => setNewtitle(e.target.value)}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Body</label>
-            <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Enter body" onChange={(e) => setNewBody(e.target.value)} />
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter body"
+              onChange={(e) => setNewBody(e.target.value)}
+            />
           </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
         </form>
-        {initialData.map(item => {
-          if(findStatus(item.id) === 'success'){
-            <div className="card" key={item.id} id={item.id.toString()}>
+
+        {initialData.map((item) => {
+            // removed unnecessary if conditions
+            <div className={`card ${findStatus(item.id) === "danger"?"bg-danger":findStatus(item.id) === "warning"?"bg-warning":"bg-success"}`} key={item.id} id={item.id}>
               <div className="card-body">
-                <h1 className="card-title">{item.title.charAt(0).toUpperCase() + item.title.slice(1)}</h1>
-                <p className="card-text">{item.body.charAt(0).toUpperCase() + item.body.slice(1)}</p>
+                <h1 className="card-title">
+                  {item.title.charAt(0).toUpperCase() + item.title.slice(1)}
+                </h1>
+                <p className="card-text">
+                  {item.body.charAt(0).toUpperCase() + item.body.slice(1)}
+                </p>
                 <span>
-                  <i className='fa fa-plus' onClick={addTodo(item.id)}></i>
+                  <i className="fa fa-plus" onClick={addTodo(item.id)}></i>
                 </span>
                 <span>
-                  <i className='fa fa-times' onClick={deleteTodo(item.id)}></i>
+                  <i className="fa fa-times" onClick={deleteTodo(item.id)}></i>
                 </span>
                 <span>
-                  <i className='fa fa-clock'></i>
+                  <i className="fa fa-clock"></i>
                   <div>{moment().format("MM ddd, YYYY HH:mm:ss a")}</div>
                 </span>
               </div>
-            </div>
+            </div>;
           }
-          if(findStatus(item.id) === 'warning'){
-              <div className="card bg-warning" key={item.id} id={item.id}>
-                <div className="card-body">
-                  <h1 className="card-title">{item.title.charAt(0).toUpperCase() + item.title.slice(1)}</h1>
-                  <p className="card-text">{item.body.charAt(0).toUpperCase() + item.body.slice(1)}</p>
-                  <span>
-                    <i className='fa fa-plus' onClick={addTodo(item.id)}></i>
-                  </span>
-                  <span>
-                    <i className='fa fa-times' onClick={deleteTodo(item.id)}></i>
-                  </span>
-                  <span>
-                  <i className='fa fa-clock'></i>
-                  <div>{moment().format("MM ddd, YYYY HH:mm:ss a")}</div>
-                </span>
-                </div>
-              </div>
-            }
-            if(findStatus(item.id) === 'danger'){
-              <div className="card bg-danger" key={item.id} id={item.id}>
-                <div className="card-body">
-                  <h1 className="card-title">{item.title.charAt(0).toUpperCase() + item.title.slice(1)}</h1>
-                  <p className="card-text">{item.body.charAt(0).toUpperCase() + item.body.slice(1)}</p>
-                  <span>
-                    <i className='fa fa-plus' onClick={addTodo(item.id)}></i>
-                  </span>
-                  <span>
-                    <i className='fa fa-times' onClick={deleteTodo(item.id)}></i>
-                  </span>
-                  <span>
-                  <i className='fa fa-clock'></i>
-                  <div>{moment().format("MM ddd, YYYY HH:mm:ss a")}</div>
-                  </span>
-                </div>
-              </div>
-            }
-        })}
+        )}
       </div>
     </React.Fragment>
-  )
+  );
 }
 export async function getStaticProps() {
-  fetch('https://jsonplaceholder.typicode.com/todos?_start=0&_limit=200')
-  .then(response => response.json())
-  .then(json => {
-    return{
-      props: {
-        data: json
-      }
-    }
-  })
+  fetch("https://jsonplaceholder.typicode.com/todos?_start=0&_limit=200")
+    .then((response) => response.json())
+    .then((json) => {
+      console.log({json})
+      return {
+        props: {
+          data: json,
+        },
+      };
+    });
 }
