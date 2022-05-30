c6db8338b0d30b6818aeb7bc1a26f6ee63800912
Irshad khan <irshad@tutorbin.com>
2022-05-30 09:18:12 +0530

    Adding todos

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
-
+import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
+  <Script src='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0-beta1/js/bootstrap.min.js' />
   return <Component {...pageProps} />
 }
 
